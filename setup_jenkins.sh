6a0833199978bef1b72b73422af8c7b351532983
Steve VandenBrink <steve@vandenbrinksoftware.com>
2024-10-15 00:10:05 -0400

    One more try tonight

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Set Jenkins UID and GID
-export JENKINS_UID=$(id -u)
-export JENKINS_GID=$(id -g)
-
-# Ensure jenkins_home volume has correct permissions
-docker run --rm -v jenkins_home:/var/jenkins_home alpine chown -R ${JENKINS_UID}:${JENKINS_GID} /var/jenkins_home
-
-# Start or restart your Docker Compose setup
-docker compose up jenkins -d
+# Start Jenkins
+docker compose up -d
 
 # Wait for Jenkins to start
 echo "Waiting for Jenkins to start..."
@@ -18,6 +11,6 @@ sleep 30
 docker compose exec jenkins bash -c "ssh-keyscan github.com >> /home/jenkins/.ssh/known_hosts"
 
 # Ensure correct permissions for .ssh directory
-docker compose exec jenkins bash -c "chown -R jenkins:jenkins /home/jenkins/.ssh && chmod 700 /home/jenkins/.ssh && chmod 600 /home/jenkins/.ssh/*"
+docker compose exec jenkins bash -c "chmod 700 /home/jenkins/.ssh && chmod 600 /home/jenkins/.ssh/*"
 
 echo "Jenkins setup complete!"
