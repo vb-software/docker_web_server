14388ce75921a2dfcab980a707a60ffe3a97da24
Steve VandenBrink <steve@vandenbrinksoftware.com>
2024-10-15 00:25:39 -0400

    update script

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start Jenkins
-docker compose up jenkins -d
+# docker compose up jenkins -d
 
-# Wait for Jenkins to start
-echo "Waiting for Jenkins to start..."
-sleep 30
+# # Wait for Jenkins to start
+# echo "Waiting for Jenkins to start..."
+# sleep 30
 
 # Add GitHub's host key to known_hosts
 docker compose exec jenkins bash -c "ssh-keyscan github.com >> /home/jenkins/.ssh/known_hosts"
