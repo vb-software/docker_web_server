63e306928497662fb7e42f25482e08c2f452ba15
Steve VandenBrink <steve@vandenbrinksoftware.com>
2024-10-15 10:11:58 -0400

    Updated setup

--- jenkins/docker-setup.sh
+++ jenkins/docker-setup.sh
@@ -5,7 +5,7 @@ add_user_to_group() {
     local user=$1
     local group=$2
     if ! id -nG "$user" | grep -qw "$group"; then
-        sudo usermod -aG $group $user
+        usermod -aG $group $user
     fi
 }
 
@@ -14,14 +14,14 @@ HOST_DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
 # If the docker group doesn't exist with the correct GID, create it
 if ! getent group docker > /dev/null 2>&1 || [ $(getent group docker | cut -d: -f3) -ne $HOST_DOCKER_GID ]; then
-    sudo groupmod -g $HOST_DOCKER_GID docker || sudo groupadd -g $HOST_DOCKER_GID docker
+    groupmod -g $HOST_DOCKER_GID docker || groupadd -g $HOST_DOCKER_GID docker
 fi
 
 # Add jenkins user to the docker group
 add_user_to_group jenkins docker
 
 # Ensure jenkins user has correct permissions
-sudo chown jenkins:jenkins /var/jenkins_home
+chown jenkins:jenkins /var/jenkins_home
 
 # Start Jenkins
-exec /sbin/tini -- /usr/local/bin/jenkins.sh "$@"
+exec su jenkins -c "/usr/local/bin/jenkins.sh"
